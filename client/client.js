77f06b4b20ae15b22ece5df3e5ac5bf13c88a42a
AryehSchoefer <aryeh.shmuel@googlemail.com>
2020-02-22 22:47:41 +0100

    backend database, frontend client form logging database info

--- client/client.js
+++ client/client.js
@@ -1,5 +1,6 @@
 const form = document.querySelector('.form')
 const loadingImg = document.querySelector('.loading')
+const API_URL = 'http://localhost:5000/mints'
 
 form.addEventListener('submit', (event) => {
     event.preventDefault()
@@ -18,7 +19,14 @@ form.addEventListener('submit', (event) => {
         message
     }
 
-    console.log(mintMessage)
+    fetch(API_URL, {
+        method: 'POST',
+        headers: {
+            'content-type': 'application/json'
+        },
+        body: JSON.stringify(mintMessage)
+    }).then(res => res.json()).then(data => console.log(data))
+
     setTimeout(() => {
         loadingImg.style.display = 'none'
         form.style.display = ''
--- server/server.js
+++ server/server.js
@@ -1,12 +1,46 @@
 const express = require('express')
+const cors = require('cors')
+const monk = require('monk')
+const Filter = require('bad-words')
 const app = express()
 
+const db = monk('localhost/mintis')
+const mints = db.get('mints')
+const filter = new Filter()
+
 app.use(express.json())
+app.use(cors())
 
 app.get('/', (req, res) => {
     res.send({
         message: 'Mintis Server 🥥'
     })
+})
+
+function isValidMint(mint) {
+    return mint.username && mint.username.toString().trim() !== '' && mint.username.toString().trim().length <= 50 &&
+        mint.message && mint.message.toString().trim() !== '' && mint.message.toString().trim().length <= 140
+}
+
+app.post('/mints', (req, res) => {
+    if (isValidMint(req.body)) {
+
+        const mint = {
+            username: filter.clean(req.body.username.toString().trim()),
+            message: filter.clean(req.body.message.toString().trim()),
+            created_date: new Date()
+        }
+
+        mints.insert(mint).then(createdMint => {
+            res.json(createdMint)
+        })
+
+    } else {
+        res.status(422)
+        res.json({
+            message: 'Invalid mint, must contain name and message'
+        })
+    }
 })
 
 app.listen(5000, () => {
