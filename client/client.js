2c94a1e98304b5b5f64d5c9eb292dfa97ac22783
AryehSchoefer <aryeh.shmuel@googlemail.com>
2020-07-28 01:48:37 +0200

    Added error handling for failed fetch request

--- client/client.js
+++ client/client.js
@@ -1,6 +1,7 @@
 const form = document.querySelector('.form')
 const loadingImg = document.querySelector('.loading')
 const messagesSection = document.querySelector('.messages')
+const serverError = document.getElementById('server-error')
 const BASE = 'http://localhost:'
 const PORT = 3000
 const API_URL = `${BASE}${PORT}`
@@ -13,6 +14,8 @@ form.addEventListener('submit', async (event) => {
     form.style.display = 'none'
     loadingImg.style.display = 'block'
 
+    serverError.style.display = 'none'
+
     const formData = new FormData(form)
 
     const username = formData.get('username')
@@ -24,15 +27,20 @@ form.addEventListener('submit', async (event) => {
         message
     }
 
-    const response = await fetch(`${API_URL}/insertMint`, {
-        method: 'POST',
-        headers: {
-            'content-type': 'application/json'
-        },
-        body: JSON.stringify(mintMessage)
-    })
-    const data = await response.json()
-    console.log(data)
+    try {
+        const response = await fetch(`${API_URL}/insertMint`, {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(mintMessage)
+        })
+        const data = await response.json()
+        console.log(data)
+    } catch (err) {
+        serverError.style.display = 'block'
+        console.log(err)
+    }
 
     setTimeout(() => {
         loadingImg.style.display = 'none'
@@ -42,36 +50,41 @@ form.addEventListener('submit', async (event) => {
 })
 
 async function loadMintis() {
-    const request = await fetch(`${API_URL}/getMintis`)
-    const messages = await request.json()
-    console.log(messages)
+    try {
+        const request = await fetch(`${API_URL}/getMintis`)
+        const messages = await request.json()
 
-    while (messagesSection.firstChild) {
-        messagesSection.removeChild(messagesSection.firstChild)
+        while (messagesSection.firstChild) {
+            messagesSection.removeChild(messagesSection.firstChild)
+        }
+
+        messages.reverse()
+        messages.forEach(message => {
+            const messageContainer = document.createElement('div')
+            messageContainer.setAttribute('class', 'message')
+
+            const username = document.createElement('h3')
+            username.setAttribute('class', 'username')
+            username.textContent = message.username
+
+            const messageText = document.createElement('p')
+            messageText.setAttribute('class', 'message-text')
+            messageText.textContent = message.message
+
+            const date = document.createElement('small')
+            date.setAttribute('class', 'date')
+            const date2 = new Date(message.date)
+            date.textContent = date2.toUTCString()
+
+            messageContainer.appendChild(username)
+            messageContainer.appendChild(messageText)
+            messageContainer.appendChild(date)
+
+            messagesSection.appendChild(messageContainer)
+        })
+
+    } catch (err) {
+        serverError.style.display = 'block'
+        console.log(err)
     }
-
-    messages.reverse()
-    messages.forEach(message => {
-        const messageContainer = document.createElement('div')
-        messageContainer.setAttribute('class', 'message')
-
-        const username = document.createElement('h3')
-        username.setAttribute('class', 'username')
-        username.textContent = message.username
-
-        const messageText = document.createElement('p')
-        messageText.setAttribute('class', 'message-text')
-        messageText.textContent = message.message
-
-        const date = document.createElement('small')
-        date.setAttribute('class', 'date')
-        const date2 = new Date(message.date)
-        date.textContent = date2.toUTCString()
-
-        messageContainer.appendChild(username)
-        messageContainer.appendChild(messageText)
-        messageContainer.appendChild(date)
-
-        messagesSection.appendChild(messageContainer)
-    })
 }
